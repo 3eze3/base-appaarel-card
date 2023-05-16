28ae09a24dd4fbdbb0cbc0194d0fd75c552d74e0
Ezequiel Cordova <ezekics33@gmail.com>
2023-05-16 16:25:25 -0500

    Add the functionality

--- dist/app/app.js
+++ dist/app/app.js
@@ -1,6 +1,35 @@
-function checkEmail(emali) {
+const button = document.querySelector(".register__button");
+button.addEventListener("click", event => {
+  event.preventDefault();
+  checkEmail();
+});
+function checkEmail() {
+  const email = document.querySelector(".register__input");
+  if (email === " ") return;
   const regex = /\w@(gmail|hotmail|yahoo).com$/gi;
-  if (regex.test(emali)) {
-    console.log("your email is ok");
+  if (regex.test(email.value)) {
+    thanks();
+  } else {
+    addRemoveBox();
   }
+}
+function addRemoveBox() {
+  const box = document.querySelector(".register__box");
+  box.classList.add("register__active");
+  setTimeout(() => {
+    box.classList.remove("register__active");
+  }, 2000);
+}
+function thanks() {
+  const content = document.querySelector(".card__content");
+  const picture = document.querySelector(".card__ilustration");
+  const box_thanks = document.querySelector(".card__thanks");
+  content.classList.add("card__content--active");
+  picture.classList.add("card__ilustration--active");
+  setTimeout(() => {
+    document.body.style.backgroundImage =
+      " linear-gradient(164deg, rgba(108,142,144,1) 50%, rgba(0,212,255,1) 85%, rgba(3,181,178,1) 96%)";
+    document.body.style.backgroundColor = "rgb(108,142,144)";
+    box_thanks.classList.add("card__thanks--active");
+  }, 250);
 }
